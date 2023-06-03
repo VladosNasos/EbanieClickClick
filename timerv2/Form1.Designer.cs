161919ab9580592c62aea55ca696918b405e1a37
VladosNasos <bezzzubkovlad@gmail.com>
2023-06-03 23:33:30 +0300

    Add project files.

---
+++ timerv2/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace timerv2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            timer1 = new System.Windows.Forms.Timer(components);
+            timer2 = new System.Windows.Forms.Timer(components);
+            label2 = new Label();
+            label1 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            timer3 = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 1;
+            timer1.Tick += timer1_Tick;
+            // 
+            // timer2
+            // 
+            timer2.Enabled = true;
+            timer2.Interval = 1000;
+            timer2.Tick += timer2_Tick;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(302, 207);
+            label2.Name = "label2";
+            label2.Size = new Size(66, 15);
+            label2.TabIndex = 0;
+            label2.Text = "My bday in";
+            label2.Click += label2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(302, 236);
+            label1.Name = "label1";
+            label1.Size = new Size(100, 15);
+            label1.TabIndex = 1;
+            label1.Text = "WinForm will end";
+            label1.Click += label1_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(302, 265);
+            label3.Name = "label3";
+            label3.Size = new Size(105, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Happy new year in";
+            label3.Click += label3_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(302, 99);
+            button1.Name = "button1";
+            button1.Size = new Size(171, 104);
+            button1.TabIndex = 3;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(529, 99);
+            button2.Name = "button2";
+            button2.Size = new Size(45, 104);
+            button2.TabIndex = 4;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Controls.Add(label2);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private Label label2;
+        private Label label1;
+        private Label label3;
+        private Button button1;
+        private Button button2;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
---
+++ timerv2/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace timerv2
+{
+    public partial class Form1 : Form
+    {
+        private int clickCount;
+        private int maxClickCount;
+        private Color[] colors = { Color.Black, Color.Red, Color.Yellow, Color.Green, Color.Cyan, Color.Blue, Color.Pink, Color.White };
+        private int currentColorIndex = 0;
+        private int a = 0;
+
+        DateTime bdayTimeLeft = new DateTime(2023, 6, 29, 0, 0, 0);
+        DateTime NewYearLeft = new DateTime(2024, 1, 1, 0, 0, 0);
+        DateTime TheEnd = new DateTime(2023, 6, 13, 19, 35, 0);
+
+        string myBday = "My bday in ";
+        string NewYear = "Happy new year in ";
+        string WF = "WF will be removed from my PC in ";
+
+  
+
+        public Form1()
+        {
+            InitializeComponent();
+            clickCount = 0;
+            maxClickCount = 0;
+
+            timer1 = new System.Windows.Forms.Timer();
+            timer2 = new System.Windows.Forms.Timer();
+            timer3 = new System.Windows.Forms.Timer();
+
+            timer1.Tick += timer1_Tick;
+            timer1.Interval = 1000;
+            timer1.Enabled = true;
+            timer1.Start();
+
+            timer2.Tick += timer2_Tick;
+            timer2.Interval = 1000;
+            timer2.Enabled = true;
+            timer2.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            clickCount++;
+            button1.Name= clickCount.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            clickCount = 0;
+
+            // Отключаем кнопку старта, чтобы предотвратить повторные запуски
+            button2.Enabled = false;
+
+            // Включаем кнопку клика, чтобы пользователь мог совершать клики
+            button1.Enabled = true;
+
+            // Запускаем таймер на 5 секунд
+            timer3.Interval = 5000; // 5 секунд
+            timer3.Tick += new EventHandler(timer_Tick);
+            timer3.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            // Останавливаем таймер
+            timer3.Stop();
+
+            // Выводим результаты
+            MessageBox.Show("Количество кликов: " + clickCount.ToString() + "\nМаксимальный рекорд: " + maxClickCount.ToString());
+
+            // Обновляем максимальный рекорд
+            if (clickCount > maxClickCount)
+            {
+                maxClickCount = clickCount;
+            }
+
+            // Сбрасываем количество кликов
+            clickCount = 0;
+
+            // Включаем кнопку старта
+            button2.Enabled = true;
+
+            // Отключаем кнопку клика
+            button1.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            a = a + 10;
+            this.Text = a.ToString();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            TimeSpan TimeRemaining = bdayTimeLeft - DateTime.Now;
+            TimeSpan TimeRemaining1 = NewYearLeft - DateTime.Now;
+            TimeSpan TimeRemaining2 = TheEnd - DateTime.Now;
+
+            label2.Text = myBday + TimeRemaining.Days + " : " + TimeRemaining.Hours + " : " + TimeRemaining.Minutes + " : " + TimeRemaining.Seconds;
+            label3.Text = NewYear + TimeRemaining1.Days + " : " + TimeRemaining1.Hours + " : " + TimeRemaining1.Minutes + " : " + TimeRemaining1.Seconds;
+            label1.Text = WF + TimeRemaining2.Days + " : " + TimeRemaining2.Hours + " : " + TimeRemaining2.Minutes + " : " + TimeRemaining2.Seconds;
+
+            Color currentColor = colors[currentColorIndex];
+            this.BackColor = currentColor;
+            this.Refresh();
+
+            currentColorIndex = (currentColorIndex + 1) % colors.Length;
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
